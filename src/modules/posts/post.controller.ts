1d341d1819b648d0d2ab661f2407e0cedcdbdc52
asraful-07 <asraful0508@gmail.com>
2025-12-28 13:09:32 +0600

    controller and service crud done

--- src/modules/posts/post.controller.ts
+++ src/modules/posts/post.controller.ts
@@ -1,13 +1,99 @@
 import { Request, Response } from "express";
-import { CreatePostService } from "./post.service";
+import {
+  CreatePostService,
+  DeletePostService,
+  GetPostService,
+  GetsPostService,
+  UpdatePostService,
+} from "./post.service";
 
 export const CreatePostController = async (req: Request, res: Response) => {
   try {
     const post = await CreatePostService(req.body);
 
-    res
-      .status(201)
-      .json({ success: true, massage: "Post create successfully", data: post });
+    res.status(201).json({
+      success: true,
+      massage: "Post created successfully",
+      data: post,
+    });
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+export const GetsPostController = async (req: Request, res: Response) => {
+  try {
+    const posts = await GetsPostService();
+
+    if (!posts) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Posts fetched successfully",
+      data: posts,
+    });
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+export const GetPostController = async (req: Request, res: Response) => {
+  try {
+    const post = await GetPostService(req.params.id as string);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
+    }
+
+    res.status(200).json({ success: true, data: post });
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+export const UpdatePostController = async (req: Request, res: Response) => {
+  try {
+    const updatedPost = await UpdatePostService(
+      req.params.id as string,
+      req.body
+    );
+
+    if (!updatedPost) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Post updated successfully",
+      data: updatedPost,
+    });
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+export const DeletePostController = async (req: Request, res: Response) => {
+  try {
+    const post = await DeletePostService(req.params.id as string);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Post deleted successfully",
+    });
   } catch (err: any) {
     res.status(500).json({ success: false, message: err.message });
   }
--- src/modules/posts/post.service.ts
+++ src/modules/posts/post.service.ts
@@ -10,7 +10,43 @@ export const CreatePostService = async (
   return result;
 };
 
-const GetsPostService = async () => {
+export const GetsPostService = async () => {
   const result = await prisma.post.findMany();
+  return result;
+};
+
+export const GetPostService = async (id: string) => {
+  const result = await prisma.post.findUnique({
+    where: { id },
+  });
+  return result;
+};
+
+//? type UpdatePostInput = {
+//   title?: string;
+//   content?: string;
+//   thumbnail?: string;
+//   isFeatured?: boolean;
+//   status?: PostStatus;
+//   tags?: string[];
+// };
+//? export const UpdatePostService = async (id: string, data: UpdatePostInput)
+
+export const UpdatePostService = async (
+  id: string,
+  data: Partial<Omit<Post, "id" | "createdAt" | "updatedAt">>
+) => {
+  const result = await prisma.post.update({
+    where: { id },
+    data,
+  });
+
+  return result;
+};
+
+export const DeletePostService = async (id: string) => {
+  const result = await prisma.post.delete({
+    where: { id },
+  });
   return result;
 };
