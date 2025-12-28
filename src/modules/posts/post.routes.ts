92ea25afa725ea1d0f27df61063dbdf3ca2ac0f8
asraful-07 <asraful0508@gmail.com>
2025-12-28 13:09:45 +0600

    post routes done

--- src/modules/posts/post.routes.ts
+++ src/modules/posts/post.routes.ts
@@ -1,8 +1,18 @@
 import express from "express";
-import { CreatePostController } from "./post.controller";
+import {
+  CreatePostController,
+  DeletePostController,
+  GetPostController,
+  GetsPostController,
+  UpdatePostController,
+} from "./post.controller";
 
 const router = express.Router();
 
 router.post("/post", CreatePostController);
+router.get("/posts", GetsPostController);
+router.get("/post/:id", GetPostController);
+router.put("/post/:id", UpdatePostController);
+router.delete("/post/:id", DeletePostController);
 
 export default router;
