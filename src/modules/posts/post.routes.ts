5568c467ee994380d7a2540ab3f5cbd52580c41d
asraful-07 <asraful0508@gmail.com>
2026-01-02 10:05:44 +0600

    routes done

--- src/modules/posts/post.routes.ts
+++ src/modules/posts/post.routes.ts
@@ -10,6 +10,7 @@ import auth, { UserRole } from "../../middleware/auth";
 
 const router = express.Router();
 
+// routes
 router.post("/post", auth(UserRole.USER, UserRole.ADMIN), CreatePostController);
 router.get("/posts", GetsPostController);
 router.get("/post/:id", GetPostController);
